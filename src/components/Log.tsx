287eb706bf210415d9c7fcf7bcf5ba3f9cc5149e
Ece Tunca <ecetunca@gmail.com>
2020-10-19 22:43:34 +0200

    added log function

--- src/components/Log.tsx
+++ src/components/Log.tsx
@@ -1,11 +1,49 @@
-import React, { FC } from 'react';
+import React, { FC, useState, useEffect } from 'react';
+import logoImageUrl from '../images/tomato-logo.png';
+
+type Log = boolean[][];
+
+export const defaultLog: Log = [
+  [false, false, false, false],
+  [false, false, false, false],
+  [false, false, false, false],
+  [false, false, false, false],
+];
+
+function getInitialLog(): Log {
+  const savedLog = localStorage.getItem('log');
+  return savedLog ? JSON.parse(savedLog) : defaultLog;
+}
 
 export const Log: FC = () => {
+  const [log, setLog] = useState(getInitialLog());
+
+  useEffect(() => {
+    localStorage.setItem('log', JSON.stringify(log));
+  }, [log]);
+
   return (
-    <div className='log' id='log'>
+    <div className='log'>
       <h4 className='log__title'>My tomato log</h4>
-      <div className='log__wrapper' id='logWrapper'></div>
-      <div className='log__clear' id='clear'>
+      <div className='log__wrapper'>
+        {log.map((logColumn, colIndex) => (
+          <div className='log__column'>
+            {logColumn.map((logIcon, iconIndex) => (
+              <img
+                className={'log__icon ' + (logIcon ? 'checked' : '')}
+                src={logoImageUrl}
+                alt='tomato logo'
+                onClick={() => {
+                  let newLog = [...log];
+                  newLog[colIndex][iconIndex] = !newLog[colIndex][iconIndex];
+                  setLog(newLog);
+                }}
+              />
+            ))}
+          </div>
+        ))}
+      </div>
+      <div className='log__clear' onClick={() => setLog(defaultLog)}>
         Clear Log
       </div>
     </div>
